3db806a8b64fc426935441bf12ff952aca0ba445
shavekevin <shavekevin@gmail.com>
2019-10-25 18:08:46 +0800

    方法耗时测试

--- iOSBasicDemo/iOSBasicDemo/iOSTech/KVOKVC/SKKVCKVOVC.m
+++ iOSBasicDemo/iOSBasicDemo/iOSTech/KVOKVC/SKKVCKVOVC.m
@@ -9,6 +9,7 @@
 #import "SKKVCKVOVC.h"
 #import "KVOModel.h"
 #import <objc/runtime.h>
+#import <mach/mach_time.h>
 
 // KVO - Key-Value- Observing 键值监听  用于监听某个对象中属性的改变
 // KVC - Key-Value- Coding 键值编码 主要用于不调用setter方法就更改或者访问对象的属性，这只是在动态地访问或更改对象的属性，而不是在编译期。
@@ -21,8 +22,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    [self setObjectMethod];
-    
+    [self sortObjectMethod];
+
 }
 
 - (void)modelValueChange {
@@ -76,16 +77,85 @@
 }
 
 - (void)setObjectMethod {
-    
+    // 集合运算符之数组
     NSMutableArray *mutableArray = [[NSMutableArray alloc]init];
     [mutableArray addObject:@"0"];
     [mutableArray addObject:@"1"];
     [mutableArray addObject:@"2"];
-    NSArray *array = @[@"name",@"label",@"test"];
-//  NSArray * namarr =   [array valueForKey:@"@unionOfArrays.name"];
-//    NSLog(@"%@",namarr);
+    [mutableArray addObject:@"4"];
+    // count 返回的是一个NSNumber对象
+    NSLog(@"计算数组的count为：%@",[mutableArray valueForKeyPath:@"@count.self"]);
+    // sum把集合中的元素改为double类型的，然后计算总和，最后返回一个值为总和的NSNumber类型的对象。类似的还有 @avg求平均值@max求最大值@min求最小值。
+    // 其中@max和@min使用的是compare来确定最大值、最小值
+    NSLog(@"计算数组中元素的sum为：%@",[mutableArray valueForKeyPath:@"@sum.self"]);
+    // 集合运算符之集合
+    NSSet *nsset = [NSSet setWithArray:mutableArray];
+    NSLog(@"计算set的count为：%@",[nsset valueForKeyPath:@"@count.self"]);
+    
+    NSMutableArray *objArray = [[NSMutableArray alloc]init];
+    
+    // 集合运算符之对象
+    KVOModel *model1 = [[KVOModel alloc]init];
+    model1.age = 10;
+    [objArray addObject:model1];
+    
+    KVOModel *model2 = [[KVOModel alloc]init];
+    model2.age = 30;
+    [objArray addObject:model2];
+    
+    KVOModel *model3 = [[KVOModel alloc]init];
+    model3.age = 50;
+    [objArray addObject:model3];
+    
+    KVOModel *model4 = [[KVOModel alloc]init];
+    model4.age = 30;
+    [objArray addObject:model4];
+    
+    //返回的是一个数组distinctUnionOfObjects 去重后根据age返回的是一个数组 数组中放的是属性的结果
+    // 个人理解这个是遍历对象然后拿出对象的对应的属性进行排序
+    NSLog(@"去重的排序: %@",[objArray valueForKeyPath:@"@distinctUnionOfObjects.age"]);
+    NSLog(@"不去重的排序: %@",[objArray valueForKeyPath:@"@unionOfObjects.age"]);
+    
 }
 
+- (void)sortObjectMethod {
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        NSMutableArray *mutableArray = [[NSMutableArray alloc]init];
+        for (NSUInteger i = 0; i < 100000; i ++) {
+            NSUInteger j = arc4random()%1000000;
+            [mutableArray addObject:@(j)];
+        }
+        uint64_t startTime = mach_absolute_time();
+        id sum = [mutableArray valueForKeyPath:@"@sum.self"];
+        NSLog(@"计算数组中元素的sum为：%@",sum);
+        [self timeOfMach:startTime];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self sortTypeMethod:mutableArray];
+        });
+    });
+}
+
+
+- (void)sortTypeMethod:(NSArray *)array {
+    uint64_t startTime = mach_absolute_time();
+    NSUInteger sum = 0;
+    for (NSUInteger i = 0; i < array.count; i ++) {
+        NSUInteger j = [array[i] intValue];
+        sum +=j;
+    }
+    NSLog(@"计算数组中元素的sum为：%@",@(sum));
+    [self timeOfMach:startTime];
+
+}
+
+- (void)methodCostTimeTest:(SEL)selector {
+    uint64_t startTime = mach_absolute_time();
+    [self performSelector:selector];
+    [self timeOfMach:startTime];
+    
+}
 // 注意 只要添加观察者都会调用这个方法(注意：只要观察者的类中实现了willChangeValueForKey或didChangeValueForKey方法 这个观察方法就不会响应)
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
@@ -97,6 +167,129 @@
         
     }
 }
+
+
+- (void)timeOfMach:(uint64_t)startTime {
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    uint64_t end = mach_absolute_time();
+    uint64_t cost = (end - startTime) * timebase.numer / timebase.denom;
+    NSLog(@"方法耗时: %f ms",(CGFloat)cost / NSEC_PER_SEC * 1000.0);
+}
+
+
+//- (void)GetCpuUsage {
+//
+//    kern_return_t kr;
+//
+//    task_info_data_t tinfo;
+//
+//    mach_msg_type_number_t task_info_count;
+//
+//    task_info_count = TASK_INFO_MAX;
+//
+//    kr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)tinfo, &task_info_count);
+//
+//    if(kr != KERN_SUCCESS) {
+//
+//        return;
+//
+//    }
+//
+//    task_basic_info_t      basic_info;
+//
+//    thread_array_t        thread_list;
+//
+//    mach_msg_type_number_t thread_count;
+//
+//    thread_info_data_t    thinfo;
+//
+//    mach_msg_type_number_t thread_info_count;
+//
+//    thread_basic_info_t basic_info_th;
+//
+//    uint32_t stat_thread =0;// Mach threads
+//
+//    basic_info = (task_basic_info_t)tinfo;
+//
+//    // get threads in the taskkr = task_threads(mach_task_self(), &thread_list, &thread_count);
+//
+//    if(kr != KERN_SUCCESS) {
+//
+//        return;
+//
+//    }
+//
+//    if(thread_count >0)
+//
+//        stat_thread += thread_count;
+//
+//    longtot_sec =0;
+//
+//    longtot_usec =0;
+//
+//    floattot_cpu =0;
+//
+//    int j;
+//
+//    for(j =0; j < thread_count; j++)
+//
+//    {
+//
+//        thread_info_count = THREAD_INFO_MAX;
+//
+//        kr = thread_info(thread_list[j], THREAD_BASIC_INFO,
+//
+//                        (thread_info_t)thinfo, &thread_info_count);
+//
+//        if(kr != KERN_SUCCESS) {
+//
+//            return;
+//
+//        }
+//
+//        basic_info_th = (thread_basic_info_t)thinfo;
+//
+//        if(!(basic_info_th->flags & TH_FLAGS_IDLE)) {
+//
+//            tot_sec = tot_sec + basic_info_th->user_time.seconds + basic_info_th->system_time.seconds;
+//
+//            tot_usec = tot_usec + basic_info_th->system_time.microseconds + basic_info_th->system_time.microseconds;
+//
+//            tot_cpu = tot_cpu + basic_info_th->cpu_usage / (float)TH_USAGE_SCALE *100.0;
+//
+//        }
+//
+//    } // for each thread
+//
+//    kr = vm_deallocate(mach_task_self(), (vm_offset_t)thread_list, thread_count *sizeof(thread_t));
+//
+//    assert(kr == KERN_SUCCESS);
+//
+//    NSLog(@"CPU Usage: %f \n", tot_cpu);
+//
+//}
+//
+//
+//- (void)GetCurrentTaskUsedMemory {
+//
+//    task_basic_info_data_t taskInfo;
+//
+//    mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
+//
+//    kern_return_t kernReturn = task_info(mach_task_self(),
+//
+//                                        TASK_BASIC_INFO, (task_info_t)&taskInfo, &infoCount);
+//
+//    if(kernReturn != KERN_SUCCESS) {
+//
+//        return;
+//
+//    }
+//
+//    NSLog(@"Memory Usage: %f", taskInfo.resident_size /1024.0/1024.0);
+//
+//}
 
 - (void)dealloc {
     // 没有add的不要移除 或者加上try catch  如果没有addobserve 就去移除 那么会crash
