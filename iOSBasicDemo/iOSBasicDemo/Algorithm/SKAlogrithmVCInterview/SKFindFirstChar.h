16dafe7c4bee6d033aeac02e3e3e12721a6d88c1
shavekevin <shavekevin@gmail.com>
2019-11-25 15:44:12 +0800

    描述更改

--- iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKFindFirstChar.h
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKFindFirstChar.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SKFindFirstChar : NSObject
-
+// 查找第一次出现的只出现一次的字符
 char findFirstChar(char *cha);
 
 @end
--- iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKFindMiddleCount.h
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKFindMiddleCount.h
@@ -9,9 +9,11 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
+// 查找无序数组当中的中位数
 @interface SKFindMiddleCount : NSObject
+
 int  findMiddleCount(int list[_Nonnull], int aLen);
+
 @end
 
 NS_ASSUME_NONNULL_END
