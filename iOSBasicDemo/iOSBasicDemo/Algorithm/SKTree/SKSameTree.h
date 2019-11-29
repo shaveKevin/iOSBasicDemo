b81c59182ea54fae83fff757f0ca8e469086cbc7
shavekevin <shavekevin@gmail.com>
2019-11-29 23:14:08 +0800

    添加面试题说明

---
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKTree/SKSameTree.h
@@ -0,0 +1,17 @@
+//
+//  SKSameTree.h
+//  iOSBasicDemo
+//
+//  Created by shavekevin on 2019/11/28.
+//  Copyright © 2019 小风. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKSameTree : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKTree/SKSameTree.m
@@ -0,0 +1,13 @@
+//
+//  SKSameTree.m
+//  iOSBasicDemo
+//
+//  Created by shavekevin on 2019/11/28.
+//  Copyright © 2019 小风. All rights reserved.
+//
+
+#import "SKSameTree.h"
+
+@implementation SKSameTree
+
+@end
