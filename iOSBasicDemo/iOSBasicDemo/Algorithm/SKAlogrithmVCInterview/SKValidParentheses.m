a4da13755cd1660daafd1973602adccab5522751
shavekevin <shavekevin@gmail.com>
2019-12-31 17:34:15 +0800

    有效的括号

--- iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKValidParentheses.m
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCInterview/SKValidParentheses.m
@@ -10,4 +10,26 @@
 
 @implementation SKValidParentheses
 
+bool isValid(char * s) {
+    // 如果字符串为空就直接返回false
+    if (s == NULL || s[0] == '\0') return  false;
+    long length = strlen(s);
+    // 如果长度不是偶数直接返回false
+    if (length %2 > 0) return  false;
+    char *stack = (char *)malloc(strlen(s))+1;
+    int top = 0;
+    for (int i = 0; s[i]; i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack[++top] = s[i];
+            
+        } else if((s[i] == (stack[top] + 1))||(s[i] == (stack[top] + 2))) {
+            top--;
+        }  else {
+            return false;
+        }
+    }
+    free(stack);
+    return  (!top);
+}
+
 @end
