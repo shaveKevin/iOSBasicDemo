aee1da5fb302cc8c1db8e9ccc2bd716d499bfd30
shavekevin <shavekevin@gmail.com>
2020-05-06 14:34:48 +0800

    update filepath

---
+++ iOSBasicDemo/iOSBasicDemo/Algorithm/SKAlogrithmVCLeetCode/Easy/SKHappy.m
@@ -0,0 +1,20 @@
+//
+//  SKHappy.m
+//  iOSBasicDemo
+//
+//  Created by shavekevin on 2020/4/30.
+//  Copyright © 2020 小风. All rights reserved.
+//
+
+#import "SKHappy.h"
+
+@implementation SKHappy
+
+bool isHappy(int n){
+
+    return YES;
+    
+}
+
+
+@end
